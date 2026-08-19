a2a40a7d154c6d8b9397ae81fa1172e052ee2d77
Ian Barbour <barbourians@users.noreply.github.com>
2026-08-19 12:52:21 +0100

    Update test_cleaning.py

--- tests/test_cleaning.py
+++ tests/test_cleaning.py
@@ -1,11 +1,3 @@
 """
 Tests for cleaning.py
 """
-
-from techmart.cleaning import (
-    remove_duplicates,
-)
-
-def test_remove_duplicates():
-    """Test remove_duplicates()."""
-    remove_duplicates
