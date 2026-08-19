b2766b01641ba9b34261a2dabf45a27d4d8dce94
Ian Barbour <barbourians@users.noreply.github.com>
2026-08-19 12:40:33 +0100

    Create test_cleaning.py

---
+++ tests/test_cleaning.py
@@ -0,0 +1,11 @@
+"""
+Tests for cleaning.py
+"""
+
+from techmart.cleaning import (
+    remove_duplicates,
+)
+
+def test_remove_duplicates():
+    """Test remove_duplicates()."""
+    remove_duplicates
